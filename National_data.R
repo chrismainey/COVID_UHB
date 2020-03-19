72266653900d7901e1352d21f66ef3b84d82392d
Chris Mainey <chrismainey@gmail.com>
2020-03-19 15:12:17 +0000

    add models

--- National_data.R
+++ National_data.R
@@ -23,6 +23,9 @@ cases$dt <- as.Date(cases$dt, "%d/%m/%Y")
 # Strip commas out of thousands (Excel, honestly...)
 cases$Cumulative <- as.integer(gsub(",", "", cases$Cumulative))
 
+# Row ide for timeperiod
+cases$day <- row(cases)[,1]
+
 
 # Quick plot of cases
 cases %>% 
@@ -60,9 +63,47 @@ plot(prop_lin, prophet_forcast)
 
 
 
-# Manual model
+# Simply exponential model
 
 
 
+mod1<- lm(log(New) ~ day, data=cases[30:47,])
+
+plot(mod1)
+summary(mod1)
+
+exp(predict(mod1, newdata=data.frame(day=48)))
 
 
+
+mod2<- lm(New ~ poly(day,2), data=cases[30:47,])
+
+plot(mod2)
+summary(mod2)
+
+predict(mod2, newdata=data.frame(day=48))
+
+
+mod3<- lm(log(New) ~ poly(day,2), data=cases[30:47,])
+
+plot(mod3)
+summary(mod3)
+
+exp(predict(mod3, newdata=data.frame(day=48)))
+
+
+new<- data.frame(days=seq(48, 60,1), cases=exp(predict(mod1, newdata=data.frame(day=seq(48,60,1)))))
+
+new2<- rbind(cbind(days = cases$day, cases=cases$New, fcast=0),
+             cbind(new, fcast=1))
+
+ggplot(new2, aes(x=days, y=cases, col=fcast))+
+  geom_line(size=1.2)+
+  labs(title= "UK nationally reported coronavirus predictions",
+       subtitle = "Data source: PHE / Gov.uk",
+       x = "Date", y = "Cases") +
+  #scale_color_brewer(type="qual")+
+  scale_y_continuous(label = comma)+
+  #scale_x_date(date_breaks = "10 day", date_labels = "%b-%d")+
+  theme(axis.text.x = element_text(angle = 45),
+        legend.title = element_blank())
