6270d05dffb550e3c8076bc4be3aa6c06e00458c
Chris Mainey <chrismainey@gmail.com>
2020-03-31 17:41:30 +0100

    Daily refresh

--- Get_international_data.R
+++ Get_international_data.R
@@ -2,22 +2,15 @@
 
 
 #these libraries are necessary
-library(readxl)
-
+library(utils)
 library(httr)
 
-#create the URL where the dataset is stored with automatic updates every day
+#download the dataset from the ECDC website to a local temporary file
+GET("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", authenticate(":", ":", type="ntlm"), write_disk(tf <- tempfile(fileext = ".csv")))
 
-url <- paste("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-",format(Sys.time(), "%Y-%m-%d"), ".xlsx", sep = "")
-
-#download the dataset from the website to a local temporary file
-
-GET(url, authenticate(":", ":", type="ntlm"), write_disk(tf <- tempfile(fileext = ".xlsx")))
-
-#read the Dataset sheet into “R”
-
-data <- read_excel(tf)
-
+#read the Dataset sheet into “R”. The dataset will be called "data".
+data <- read.csv(tf, stringsAsFactors = FALSE)
+names(data)[[1]] <- "dateRep"
 
 
 
@@ -25,14 +18,15 @@ data <- read_excel(tf)
 # Let's have a look and plot
 summary(data)
 head(data)
-data$`Countries and territories` <- factor(data$`Countries and territories`)
+#data$`Countries and territories` <- factor(data$CountriesAndTerritories)
+data$countriesAndTerritories <- factor(data$countriesAndTerritories)
 
 # See which countries are in there
-levels(data$`Countries and territories`)
+levels(data$countriesAndTerritories)
 
 #take data back one day, as it reported a day later accoridng to DM and DG 
-data$DateRep = as.Date(data$DateRep)
-data$DateRep = data$DateRep - 1
+data$dateRep = as.Date(data$dateRep, format = "%d/%m/%Y")
+data$dateRep = data$dateRep - 1
 
 # Load packages
 lapply(c("dplyr", "tidyr", "Cairo", "ggplot2", "scales"), require, character.only=TRUE)
@@ -51,13 +45,13 @@ theme_set(
 data %>% 
   #mutate(DateRep = as.Date(DateRep)) %>% 
   #mutate(DateRep2 = DateRep - 1) %>% 
-  filter(`Countries and territories` %in% c("United_Kingdom", "Italy")) %>% 
-  select(DateRep, Cases, Deaths, Country = `Countries and territories`) %>% 
-  pivot_longer(cols = c(Cases, Deaths)
+  filter(countriesAndTerritories %in% c("United_Kingdom", "Italy")) %>% 
+  select(dateRep, cases, deaths, Country = countriesAndTerritories) %>% 
+  pivot_longer(cols = c(cases, deaths)
                , values_to = "Count"
                , names_to = "Measure"
                ) %>% 
-  ggplot(aes(x=DateRep, y=Count, col=Country, linetype=Measure))+
+  ggplot(aes(x=dateRep, y=Count, col=Country, linetype=Measure))+
   geom_line(size=1)+
  labs(title= "International coronavirus infections",
        subtitle = "Data source: Ecdc daily",
@@ -71,36 +65,40 @@ data %>%
 
 # Apply same forecasting methods as national
 data %>% 
-  filter(`Countries and territories` %in% c("United_Kingdom")) %>% 
-  arrange(DateRep) %>% 
-  mutate(cumulative = cumsum(Deaths)) %>% 
+  filter(countriesAndTerritories %in% c("United_Kingdom")) %>% 
+  arrange(dateRep) %>% 
+  mutate(cumulative = cumsum(deaths)) %>% 
   as.data.frame()
 
+data %>% 
+  filter(countriesAndTerritories %in% c("United_Kingdom")) %>% 
+  summarise(sum(deaths))
 # Set at tsibble
 
 library(fable)
 library(dplyr)
 
 cases_ts <- data %>% 
-  mutate(DateRep = as.Date(DateRep)) %>% 
+  #mutate(DateRep = as.Date(DateRep)) %>% 
   #filter(`Countries and territories` %in% c("United_Kingdom", "Italy")) %>%
-  filter(`Countries and territories` %in% c("United_Kingdom")) %>% 
-  select(DateRep, Cases, Deaths, Country = `Countries and territories`) %>% 
+  #filter(`Countries and territories` %in% c("United_Kingdom")) %>% 
+  filter(countriesAndTerritories %in% c("United_Kingdom")) %>% 
+  select(dateRep, cases, deaths, Country = countriesAndTerritories) %>% 
   # pivot_wider(id_cols = DateRep,
   #             names_from = Country,
   #             values_from = c(Cases, Deaths)
   #             ) %>% 
-  as_tsibble(index=DateRep,
+  as_tsibble(index=dateRep,
              key=Country)
 
 # Use autoplot
-cases_ts %>% autoplot(Cases)
-cases_ts %>% autoplot(Deaths)
+cases_ts %>% autoplot(cases)
+cases_ts %>% autoplot(deaths)
 
 
 # FIt various models, as a shot in the dark (ETS is exponential smoothing)
 cases_ts_fit <- cases_ts %>% 
-  mutate(dts = Deaths + 1) %>% 
+  mutate(dts = deaths + 1) %>% 
   model( #mean = MEAN(Cases)
     #, naive = NAIVE(Cases)
     #, snaive = SNAIVE(Cases, lag="day")
@@ -118,7 +116,7 @@ cases_ts_fit <- cases_ts %>%
     #, holt_winterMN_case = ETS(Cases ~ error("A")+trend("M")+season("N"))
     
      #ets_death = ETS(log(dts))
-     ses_death = ETS(log(Deaths + 1) ~ error("A")+trend("A")+season("N"))
+     ses_death = ETS(log(deaths + 1) ~ error("A")+trend("A")+season("N"))
     #, sesM_death = ETS(log(dts) ~ error("A")+trend("M")+season("A"))
     #, sesMN_death = ETS(log(dts) ~ error("A")+trend("M")+season("N"))
     #, holt_winter_death = ETS(log(dts) ~ error("A")+trend("A")+season("A"))
@@ -166,7 +164,7 @@ cases_ts_forcast5d %>%
 
 
 data %>% 
-mutate(DateRep = as.Date(DateRep)) %>% 
+#mutate(DateRep = as.Date(dateRep)) %>% 
   filter(`Countries and territories` %in% c("United_Kingdom")) %>% 
   select(Country = `Countries and territories`, DateRep, Deaths) %>% 
   
@@ -184,3 +182,14 @@ mutate(DateRep = as.Date(DateRep)) %>%
   scale_x_date(date_breaks = "10 day", date_labels = "%b-%d")+
   theme(axis.text.x = element_text(angle = 45),
         legend.title = element_blank())
+
+
+
+
+######## Version using day since 10 cases
+
+# Add cumulative deaths and cases
+
+data %>% 
+  mutate(Cumulative_cases = cumsum(cases),
+         
